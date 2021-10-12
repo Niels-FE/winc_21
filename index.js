08368d5f88b98c592493f850fe7a784bcc547192
Niels-FE <89984377+Niels-FE@users.noreply.github.com>
2021-10-12 15:59:26 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const pizzaMaking = function(){
+    console.log('i maked a pizza');
+};
+pizzaMaking();
+const sushiMaking = function(){
+    console.log('i get you a sushi');
+};
+sushiMaking();
+sushiMaking();
